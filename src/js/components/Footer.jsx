a3836855621703b3d1093fe5d3b3e65bf5f05dd2
TheDude70 <dude.the.man@gmail.com>
2020-01-30 13:16:47 +0800

    Update Footer.jsx

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,7 +3,12 @@ import React from "react";
 class Footer extends React.Component {
   render() {
     return (
-      <footer className={"bg-smoke-400 bottom-0 py-2 w-full" + (this.props.authenticated ? '' : ' absolute')}>
+      <footer
+        className={
+          "bg-smoke-400 bottom-0 py-2 w-full" +
+          (this.props.authenticated ? "" : " absolute")
+        }
+      >
         <p className="text-center text-white text-xs">
           Copyright &copy; {new Date().getFullYear()} {process.env.APP_NAME}.
           All rights reserved.
